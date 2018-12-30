37fa959c4729c10635c9e155cf19465e2d4f354b
douglassllc <doug@douglassllc.com>
2018-12-30 21:05:46 +0100

    Verified usage on all platforms. Added cleanup script. Updated .gitignore to exclude additional DLLs found in bin/ directory after install

--- build/index.sh
+++ build/index.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 set +e
 cd $(dirname $0)
 
@@ -11,52 +11,76 @@ download () {
 	curl -L -# -A 'https://github.com/douglassllc/@douglassllc/mp4box-static' -o $2 $1
 }
 
-echo 'windows x64'
-echo '  downloading from download.tsi.telecom-paristech.fr'
+echo 'Windows x64'
+echo '-> downloading from download.tsi.telecom-paristech.fr'
 download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/win64/gpac-0.7.2-DEV-latest-master-x64.exe' win32-x64.exe
-echo '  moving'
-cp win32-x64.exe ../bin/win32/x64/MP4Box.exe
-
-echo 'windows ia32'
-echo '  downloading from download.tsi.telecom-paristech.fr'
-download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/win32/gpac-0.7.2-DEV-latest-master-win32.exe' win32-ia32.exe
-echo '  moving'
-cp win32-ia32.exe ../bin/win32/ia32/MP4Box.exe
-
-echo 'linux x64'
-echo '  downloading from download.tsi.telecom-paristech.fr'
-download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/linux64/gpac/gpac_0.7.2-DEV-latest-master_amd64.deb' linux-x64.deb
-echo '  extracting'
-mkdir -p tmp
-cp linux-x64.deb tmp
-pushd tmp
-ar -x linux-x64.deb
-$tar_exec -x --strip-components 1 -f data.tar.xz --wildcards '*/MP4Box'
-echo '  moving'
-cp usr/bin/MP4Box ../../bin/linux/x64/
+echo '-> extracting'
+mkdir -p win32-x64-unpacked
+cp win32-x64.exe win32-x64-unpacked/setup.exe
+pushd win32-x64-unpacked
+7z x setup.exe
+echo '-> moving'
+cp mp4box.exe ../../bin/win32/x64/MP4Box.exe
+cp js.dll ../../bin/win32/x64/
+cp libcryptoMD.dll ../../bin/win32/x64/
+cp libsslMD.dll ../../bin/win32/x64/
 popd
+rm -rf win32-x64-unpacked
 
-echo 'linux ia32'
-echo '  downloading from download.tsi.telecom-paristech.fr'
+echo 'Windows ia32'
+echo '-> downloading from download.tsi.telecom-paristech.fr'
+download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/win32/gpac-0.7.2-DEV-latest-master-win32.exe' win32-ia32.exe
+echo '-> extracting'
+mkdir -p win32-ia32-unpacked
+cp win32-ia32.exe win32-ia32-unpacked/setup.exe
+pushd win32-ia32-unpacked
+7z x setup.exe
+echo '-> moving'
+cp mp4box.exe ../../bin/win32/ia32/MP4Box.exe
+cp js.dll ../../bin/win32/ia32/
+cp libcryptoMD.dll ../../bin/win32/ia32/
+cp libsslMD.dll ../../bin/win32/ia32/
+popd
+rm -rf win32-ia32-unpacked
+
+echo 'Linux ia32'
+echo '-> downloading from download.tsi.telecom-paristech.fr'
 download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/linux32/gpac/gpac_0.7.2-DEV-latest-master_i386.deb' linux-ia32.deb
-echo '  extracting'
-cp linux-ia32.deb tmp
-pushd tmp
+echo '-> extracting'
+mkdir -p linux-ia32-unpacked
+cp linux-ia32.deb linux-ia32-unpacked
+pushd linux-ia32-unpacked
 ar -x linux-ia32.deb
 $tar_exec -x --strip-components 1 -f data.tar.xz --wildcards '*/MP4Box'
-echo '  moving'
+echo '-> moving'
 cp usr/bin/MP4Box ../../bin/linux/ia32/
 popd
+rm -rf linux-ia32-unpacked
 
-echo 'darwin x64'
-echo '  downloading from evermeet.cx'
-download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/macosx_64/gpac-0.7.2-DEV-latest-master.pkg' darwin-x64-mp4box.pkg
-echo '  extracting'
-cp darwin-x64-mp4box.pkg tmp
-pushd tmp
-pkgutil --expand-full darwin-x64-mp4box.pkg darwin
-echo '  moving'
-cp darwin/tmppkg.pkg/Payload/GPAC.app/Contents/MacOS/MP4Box ../../bin/darwin/x64/
+echo 'Linux x64'
+echo '-> downloading from download.tsi.telecom-paristech.fr'
+download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/linux64/gpac/gpac_0.7.2-DEV-latest-master_amd64.deb' linux-x64.deb
+echo '-> extracting'
+mkdir -p linux-x64-unpacked
+cp linux-x64.deb linux-x64-unpacked
+pushd linux-x64-unpacked
+ar -x linux-x64.deb
+$tar_exec -x --strip-components 1 -f data.tar.xz --wildcards '*/MP4Box'
+echo '-> moving'
+cp usr/bin/MP4Box ../../bin/linux/x64/
 popd
+rm -rf linux-x64-unpacked
 
-rm -rf tmp
+echo 'Darwin x64'
+echo '-> downloading from download.tsi.telecom-paristech.fr'
+download 'https://download.tsi.telecom-paristech.fr/gpac/new_builds/macosx_64/gpac-0.7.2-DEV-latest-master.pkg' darwin-x64-mp4box.pkg
+echo '-> extracting'
+mkdir -p darwin-x64-unpacked
+cp darwin-x64-mp4box.pkg darwin-x64-unpacked
+pushd darwin-x64-unpacked
+pkgutil --expand-full darwin-x64-mp4box.pkg darwin
+echo '-> moving'
+cp darwin/tmppkg.pkg/Payload/GPAC.app/Contents/MacOS/MP4Box ../../bin/darwin/x64/
+cp -rp darwin/tmppkg.pkg/Payload/GPAC.app/Contents/MacOS/lib ../../bin/darwin/x64/
+popd
+rm -rf darwin-x64-unpacked
